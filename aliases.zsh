ef7ba087de843cd57c520cc64eb09a35a5be4000
Sean Carey <sean.carey@foxsports.com.au>
2017-08-09 18:05:00 +1000

    Updates

--- aliases.zsh
+++ aliases.zsh
@@ -22,7 +22,7 @@ alias npmraf="rimraf node_modules && npm install"
 alias reload='source ~/.zshrc'
 alias repos="cd /Users/sean.carey/repos"
 alias ssh-clipboard="pbcopy < ~/.ssh/id_rsa.pub && echo \"Copied ~/.ssh/id_rsa.pub to clipboard: \n\" && cat ~/.ssh/id_rsa.pub"
-alias ssh-reset="rm ~/.ssh/environment-IT030850"
+alias ssh-reset="rm ~/.ssh/environment-*"
 alias subl="open /Applications/Sublime\ Text.app"
 alias version="node -p \"require('./package.json').version\""
 
@@ -30,67 +30,15 @@ alias vip-up="docker-compose -f ~/repos/vip-quickstart-mirror/docker-compose.dev
 alias vip-down="docker-compose -f ~/repos/vip-quickstart-mirror/docker-compose.dev.yml down"
 
 # disabling artifactory
-alias npm-foxsports='npm config set registry https://artifactory.foxsports.com.au/api/npm/npm-remote/'
-alias npm-public='npm config set registry https://registry.npmjs.org'
+alias npm-foxsports='npmrc default'
+alias npm-public='npmrc public'
 
 # reload fonts when corrupted
 alias fonts-repair='sudo atsutil databases -remove; atsutil server -shutdown'
 alias chmod-remove-x-recursive='sudo chmod -R -x * && sudo chmod -R +X *'
 
-# personal
-alias ssh-mediatemple='ssh root@bgsh-qwst.accessdomain.com'
-alias ssh-heymamashey="ssh wordpress@ec2-52-62-37-136.ap-southeast-2.compute.amazonaws.com"
-
 #echo "AD Password: " && read AD_PASSWORD && echo "sean.carey:$AD_PASSWORD" && unset -v AD_PASSWORD
 
-# WIP
-function v {
-    if (($# == 0))
-        then
-            echo 'Please specify npm version (patch, minor, major) or specific version'
-            echo 'e.g. "v 1.12.1" or "v major"'
-            return 1;
-    fi
+[ -f ~/.oh-my-zsh/custom/functions/version.zsh ] && source ~/.oh-my-zsh/custom/functions/version.zsh
 
-    echo "Updating version: `version` to version: $1"
-    echo 'Going to run the following commands:'
-    echo '```'
-    echo '    git fetch'
-    echo '    git checkout develop && git reset --hard origin/develop'
-    echo "    npm version $1"
-    echo '    git checkout master && git reset --hard origin/master'
-    echo '    git merge develop'
-    echo '```'
-
-    echo "I'll ask again before pushing anything to remote. Go ahead and do it? (y/n)"
-    while true; do
-        read doit
-        case $doit in
-            [Yy]* )
-                echo "git fetch && git checkout develop && git reset --hard origin/develop && npm version $1 && git checkout master && git reset --hard origin/master && git merge develop"
-                break;;
-            [Nn]* )
-                echo 'Aborting...';
-                return 1;;
-            * )
-                echo 'Please choose y or n';;
-        esac
-    done
-
-    echo "New version: `version` tagged. Push develop & master to remote? (y/n)"
-    while true; do
-        read yn
-        case $yn in
-            [Yy]* )
-                echo 'git push --tags && git push && git checkout develop && git push';
-                break;;
-            [Nn]* )
-                echo 'Trying to reset changes on local master/develop';
-                echo 'git co develop && git tag -l | xargs git tag -d && git fetch --tags'; # reset tags on develop
-                echo 'git co master && git reset --hard origin/master && git tag -l | xargs git tag -d && git fetch --tags'; # reset master to origin and reset tags
-                return 1;;
-            * )
-                echo 'Please choose y or n';;
-        esac
-    done
-}
+# git config --global url."https://github.com".insteadOf git://github.com
--- key-bindings.zsh
+++ key-bindings.zsh
@@ -1,5 +1,4 @@
-bindkey '^[^[[D' backward-word         # alt+left
-bindkey '^[^[[C' forward-word          # alt+right
-bindkey '^[¯' beginning-of-line        # cmd+left
-bindkey '^[˘' end-of-line              # cmd+right
-bindkey '^[Î' backward-delete-word     # iterm2 passing through special char for cmd+delete
+# https://stackoverflow.com/questions/6205157/iterm-2-how-to-set-keyboard-shortcuts-to-jump-to-beginning-end-of-line
+bindkey "^U" backward-kill-line # changes hex 0x15 to delete everything to the left of the cursor, rather than the whole line
+bindkey "^X\\x7f" backward-kill-line # binds hex 0x18 0x7f with deleting everything to the left of the cursor
+bindkey "^X^_" redo # adds redo
